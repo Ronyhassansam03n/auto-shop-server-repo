9da49a1b39199d0b900005f6f304897703219d69
ronyhassansam03n <ronyhassansam03n@gmail.com>
2022-11-28 17:18:19 +0600

    pay done

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 
+const stripe = require("stripe")(process.env.STRIPE_KEY);
 
 app.use(cors());
 app.use(express.json());
@@ -69,6 +70,16 @@ async function run() {
 
         })
 
+        //pay 
+
+        app.get('/bookings/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const booking = await bookingCarCollection.findOne(query)
+            res.send(booking)
+
+        })
+
         app.post('/bookings', async (req, res) => {
             const booking = req.body
             console.log(booking)
@@ -76,6 +87,32 @@ async function run() {
             res.send(result)
 
 
+        })
+
+        app.post('/create-payment-intent', async (req, res) => {
+            const booking = req.body;
+            const price = booking.price;
+            const amount = price * 100;
+
+            const paymentIntent = await stripe.paymentIntents.create({
+
+                currency: "usd",
+                amount: amount,
+                "payment_method_types": [
+                    "card"
+                ]
+            })
+            res.send({
+                clientSecret: paymentIntent.client_secret,
+            });
+        })
+
+
+        app.delete('bookings/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) }
+            const result = await bookingCarCollection.deleteOne(filter);
+            res.send(result);
         })
 
 
